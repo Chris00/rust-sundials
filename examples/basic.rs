ba07d664bcd0e7b386cbadcb653d4de53b9a1313
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2022-11-30 10:43:30 +0100

    Add a basic example

---
+++ examples/basic.rs
@@ -0,0 +1,12 @@
+use sundials::CVode;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let f = |t, u: &[f64; 1], du: &mut [f64; 1]| {
+        *du = [1.]
+    };
+    let mut ode = CVode::adams(f, 0., &[0.])?;
+    let mut u1 = [f64::NAN];
+    ode.solve(1., &mut u1);
+    assert_eq!(u1[0], 1.);
+    Ok(())
+}
